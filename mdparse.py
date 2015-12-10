53a94ff822d657fabe4b018f2a74c4ecf572e965
Michael Andrew Auer <mauermcx@gmail.com>
2015-12-10 09:43:08 -0500

    err() now returns values instead of printing them, split parse() into parse() and parse_wrapper()

--- mdparse.py
+++ mdparse.py
@@ -9,12 +9,12 @@ import os.path
 def main():
     # Clear terminal window on osx/linux
     os.system('clear')
-    
+
     # Check if there are enough cl args and if the .md file exists and if the file is a markdown file
     if len(sys.argv) > 1:
         # if an invalid file was passed (ie not markdown)
         if (sys.argv[1][-3:] != '.md'):
-            return err(0)
+            return print(err(0, sys.argv[1]))
         # check if an html file of the same name exists
         elif os.path.exists(sys.argv[1][:-3] + '.html'):
             # if so prompt user if they want to override the previous itteration of the file
@@ -23,37 +23,41 @@ def main():
                 try:
                     with io.open(sys.argv[1]) as f:
                         # pass contents of file to the parser code
-                        parse(f.readlines())
+                        parse_wrapper(f.readlines(), sys.argv[1])
                     return f.close()
                 except:
-                    return err(1)
+                    return print(err(1, sys.argv[1]))
             else:
                 return print("Not overwriting the file. Exiting now.")
 
     elif len(sys.argv) == 1:
-        return err(2)
+        return print(err(2))
     else:
         return print('Not sure what you did yet')
 
-def parse(md):
-    # parse markdown to html
+def parse_wrapper(md, filename):
     # write to an html file of the same name
-    with io.open('note.html', 'w') as f:
+    with io.open(filename[:-3] + '.html', 'w') as f:
         for line in md:
-            f.write(line)
-        f.close()
-    return
+            f.write(parse(line))
+    return f.close()
 
-def err(id):
+def parse(line):
+    # parse markdown to html
+    return line
+
+def err(id, filename=None):
     # Not a markdown file
     if id == 0:
-        return print(sys.argv[1] + ' is not a markdown file')
+        return sys.argv[1] + ' is not a markdown file'
     # if the file just doesn't exist
     elif id == 1:
-        return print(sys.argv[1] + ': this file does not exist')
+        return sys.argv[1] + ': this file does not exist'
     # if no file was passed
     elif id == 2:
-        return print('No file was passed')
+        return 'No file was passed'
+    else:
+        return id + ' is not a valid ID'
 
 if __name__ == '__main__':
     main()
