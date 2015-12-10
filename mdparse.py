816fa3927503e68436af5b93993003352bd19ef6
Michael Andrew Auer <mauermcx@gmail.com>
2015-12-10 09:30:04 -0500

    Check if file exists and clear terminal window on program start

--- mdparse.py
+++ mdparse.py
@@ -3,24 +3,32 @@
 # imports
 import sys
 import io
+import os
+import os.path
 
 def main():
+    # Clear terminal window on osx/linux
+    os.system('clear')
+    
     # Check if there are enough cl args and if the .md file exists and if the file is a markdown file
     if len(sys.argv) > 1:
         # if an invalid file was passed (ie not markdown)
         if (sys.argv[1][-3:] != '.md'):
             return err(0)
-        try:
-            with io.open(sys.argv[1]) as f:
-                parse(f.readlines())
-            return f.close()
-        except:
-            return err(1)
-
-
         # check if an html file of the same name exists
-        # if so prompt user if they want to override the previous itteration of the file
-        # pass contents of file to the parser code
+        elif os.path.exists(sys.argv[1][:-3] + '.html'):
+            # if so prompt user if they want to override the previous itteration of the file
+            will_overwrite = input('The file ' + sys.argv[1][:-3] + '.html already exists. Do you want to overwrite it? (y/n):  ')
+            if will_overwrite.strip().lower() == 'y':
+                try:
+                    with io.open(sys.argv[1]) as f:
+                        # pass contents of file to the parser code
+                        parse(f.readlines())
+                    return f.close()
+                except:
+                    return err(1)
+            else:
+                return print("Not overwriting the file. Exiting now.")
 
     elif len(sys.argv) == 1:
         return err(2)
