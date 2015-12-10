2e99a360041f0a59dfd5268a34a41056f33fb04c
Michael Andrew Auer <mauermcx@gmail.com>
2015-12-10 09:14:41 -0500

    Modular error reporting and add basic control flow shell

--- mdparse.py
+++ mdparse.py
@@ -7,19 +7,25 @@ import io
 def main():
     # Check if there are enough cl args and if the .md file exists and if the file is a markdown file
     if len(sys.argv) > 1:
+        # if an invalid file was passed (ie not markdown)
+        if (sys.argv[1][-3:] != '.md'):
+            return err(0)
+        try:
+            with io.open(sys.argv[1]) as f:
+                parse(f.readlines())
+            return f.close()
+        except:
+            return err(1)
+
+
         # check if an html file of the same name exists
         # if so prompt user if they want to override the previous itteration of the file
         # pass contents of file to the parser code
-        with io.open(sys.argv[1]) as f:
-            parse(f.readlines())
-        f.close()
+
+    elif len(sys.argv) == 1:
+        return err(2)
     else:
-        # if no file was passed
-        if len(sys.argv) == 1:
-            print("No file was passed")
-        # if an invalid file was passed (ie not markdown)
-        # if the file just doesn't exist
-    return
+        return print('Not sure what you did yet')
 
 def parse(md):
     # parse markdown to html
@@ -29,6 +35,17 @@ def parse(md):
             f.write(line)
         f.close()
     return
+
+def err(id):
+    # Not a markdown file
+    if id == 0:
+        return print(sys.argv[1] + ' is not a markdown file')
+    # if the file just doesn't exist
+    elif id == 1:
+        return print(sys.argv[1] + ': this file does not exist')
+    # if no file was passed
+    elif id == 2:
+        return print('No file was passed')
 
 if __name__ == '__main__':
     main()
